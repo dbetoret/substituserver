d304d6baa3d570d3ce870727d359ca854316f444
David Betoret Cortés <dbetoret@gmail.com>
2022-12-22 01:48:29 +0100

    actualitza elements de seguretat

--- substituserver/settings.py
+++ substituserver/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if os.path.isfile('./substituserver/secret/secret_key.txt'):
-    with open('./substituserver/secret/secret_key.txt') as f:
-        SECRET_KEY = f.read().strip()
+# if os.path.isfile('./substituserver/secret/secret_key.txt'):
+#     with open('./substituserver/secret/secret_key.txt') as f:
+#         SECRET_KEY = f.read().strip()
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
